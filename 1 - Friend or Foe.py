baec1f48dc49d1c2d596578c62681cdd8f4b4a22
thomaslee01 <thomas.lee@uwaterloo.ca>
2021-05-20 17:35:21 -0400

    added level

--- 1 - Friend or Foe.py
+++ 1 - Friend or Foe.py
@@ -1,4 +1,6 @@
 '''
+7 kyu
+
 Make a program that filters a list of strings and returns a list with only your friends name in it.
 
 If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...

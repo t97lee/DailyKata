89011a41f50a70a919d2df2b742bc5c533ffe778
thomaslee01 <thomas.lee@uwaterloo.ca>
2021-05-20 17:33:35 -0400

    initial commit

---
+++ 1 - Friend or Foe.py
@@ -0,0 +1,18 @@
+'''
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e. friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+'''
+
+def friend(x):
+    #Code
+    new_list = []
+    for name in x:
+        if len(name) == 4:
+            new_list.append(name)
+    return(x)

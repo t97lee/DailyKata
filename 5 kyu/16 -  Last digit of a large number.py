9359032a64f8c578553e053b0ddf43047e5866c0
thomaslee01 <t97lee@uwaterloo.ca>
2021-06-18 09:04:28 -0400

    added 5kyu folder/problem

---
+++ 5 kyu/16 -  Last digit of a large number.py
@@ -0,0 +1,10 @@
+'''
+5 kyu
+
+Define a function that takes in two non-negative integers a and b and returns the last decimal digit of a ** b.
+
+Note that aaa and bbb may be very large!
+
+'''
+def last_digit(n1, n2):
+    return pow(n1, n2, 10) #using modular arithmetic

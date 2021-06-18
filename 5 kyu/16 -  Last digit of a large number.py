30695869a4cfd2c8ea106bfd3cb7bb95c1c4e089
thomaslee01 <t97lee@uwaterloo.ca>
2021-06-18 09:05:29 -0400

    updated docstring

--- 5 kyu/16 -  Last digit of a large number.py
+++ 5 kyu/16 -  Last digit of a large number.py
@@ -3,7 +3,7 @@
 
 Define a function that takes in two non-negative integers a and b and returns the last decimal digit of a ** b.
 
-Note that aaa and bbb may be very large!
+Note that a and b may be very large!
 
 '''
 def last_digit(n1, n2):
